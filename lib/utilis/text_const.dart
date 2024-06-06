21212661a9eb44f83a75eaea81dd5f36f346fdeb
vishag002 <vvishag477@gmail.com>
2024-06-06 11:31:38 +0530

    text_const

---
+++ lib/utilis/text_const.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TextConst {
+  static TextStyle inputStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 18,
+    fontWeight: FontWeight.w300,
+  );
+  static TextStyle userContainer = TextStyle(
+    color: Colors.purple.shade50,
+  );
+}
